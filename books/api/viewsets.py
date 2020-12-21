7e098903c02983ad93459194d52c8a52f970bee2
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2020-12-20 23:22:25 -0300

    Configured serializers e viewsets for author

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -5,3 +5,7 @@ from books import models
 class BooksViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.BooksSerializer
     queryset = models.Books.objects.all()
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.AuthorSerializer
+    queryset = models.Author.objects.all()
