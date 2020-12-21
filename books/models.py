9e704e49cc7a9a7d1e00da274ddae388f72c4679
Gabriel Jacobina <sgabrieljacobina@gmail.com>
2020-12-20 23:18:30 -0300

    new model: author

--- books/models.py
+++ books/models.py
@@ -9,10 +9,15 @@ def upload_image_book(instance, filename):
 class Books(models.Model):
     id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', related_name='books')
     release_year = models.IntegerField()
     state =  models.CharField(max_length=50)
     pages = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     create_at = models.DateField(auto_now_add=True)
     image = models.ImageField(upload_to=upload_image_book, blank=True, null=True)
+
+
+class Author(models.Model):
+    id_author = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    nome = models.CharField(max_length=255)
